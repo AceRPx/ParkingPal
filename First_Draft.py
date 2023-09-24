458b9596affb04936035e8e3c87816f29fdac795
Taylj138 <145611614+Taylj138@users.noreply.github.com>
2023-09-24 13:46:46 -0400

    Add files via upload

---
+++ First_Draft.py
@@ -0,0 +1,38 @@
+import random
+
+# Define available parking lots and their capacities
+parking_lots = {
+    'Red': 10,
+    'Blue': 15,
+    'Green': 12,
+}
+
+# Function to display available parking lots
+def display_available_lots():
+    print("Available Parking Lots:")
+    for color, spaces in parking_lots.items():
+        print(f"{color} - {spaces} spaces")
+
+# Function to park in a selected lot
+def park_car(selected_lot):
+    if selected_lot in parking_lots and parking_lots[selected_lot] > 0:
+        parking_lots[selected_lot] -= 1
+        print(f"You have parked your car in the {selected_lot} lot.")
+    else:
+        print("Sorry, the selected lot is full or invalid.")
+
+# Main loop
+while True:
+    display_available_lots()
+
+    # Get user input
+    selected_color = input("Select a parking lot color (or 'quit' to exit): ").capitalize()
+
+    if selected_color == 'Quit':
+        break
+
+    park_car(selected_color)
+
+# Final display of available parking lots
+print("Thank you for using the parking lot app.")
+display_available_lots()
